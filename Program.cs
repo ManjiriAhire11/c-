e7cd531eeec0d717c9504cd45ab9abb40e82bd5d
ManjiriAhire11 <90183145+ManjiriAhire11@users.noreply.github.com>
2021-09-07 23:24:08 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2Ass_1
+{
+  public  class Program
+    {
+       public static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Round(12.5));
+            Console.WriteLine(Math.Round(10.5));
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
+
+//Output:
+//12
+//10
